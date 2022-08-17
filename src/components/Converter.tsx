aa403fcc0bbd7469a657f36cf2e8d0e979996de7
Sergio00001 <digtyarsergio@gmail.com>
2022-08-17 11:34:40 +0300

    add background

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -4,6 +4,8 @@ import { useFetching } from '../hooks/useFetching';
 import { useReverse } from '../hooks/useReverse';
 import { useOptionsAndCalcResult } from '../hooks/useOptionsAndCalcResult';
 
+import background from '../assets/background.jpg'
+
 export const Converter = () => {
     const { isLoading, currOptions, date, fromOption, toOption, setFromOption, setToOption } = useFetching()
     const reverseExchange = useReverse({ fromOption, toOption, setFromOption, setToOption })
@@ -11,34 +13,37 @@ export const Converter = () => {
 
     if (isLoading) {
         return (
-            <div className='flex justify-center items-center w-screen h-screen bg-green-100'>
+            <div className='flex justify-center items-center w-screen h-screen bg-black'>
                 <div className="loader"><div></div><div></div><div></div><div></div></div>
             </div>
         )
     }
 
     return (
-        <div className="flex justify-center items-center w-screen h-screen bg-green-100">
-            <div className='sm:shadow-xl rounded py-3 px-5 sm:py-6 sm:px-10 bg-white'>
-                <h1 className='text-center mb-4 text-4xl'>Currency Converter</h1>
-                <hr />
-                <div className='text-center'>
-                    <h3 className='my-6 text-4xl font-bold sm:text-7xl'>{fromInput} {fromOption === 'From' ? '' : selectedFrom?.cc}</h3>
-                    <h3 className='text-2xl sm:text-3xl'>IS EQUIVALENT TO</h3>
-                    <h1 className='my-6 text-4xl font-bold sm:text-7xl'>{result} {toOption === 'To' ? '' : selectedTo?.cc}</h1>
-                    <h4 className='mt-6 sm:mt-0 sm:my-6 text-xl sm:text-xl'> As of {date}</h4>
+        <>
+            <img src={background} alt="background" className='background__img' />
+            <div className="converter flex justify-center items-center w-screen h-screen">
+                <div className='sm:shadow-xl rounded py-3 px-5 sm:py-6 sm:px-10 bg-white'>
+                    <h1 className=' text-center mb-4 text-4xl'>Currency Converter</h1>
+                    <hr />
+                    <div className='text-center'>
+                        <h3 className='my-6 text-4xl font-bold sm:text-7xl'>{fromInput} {fromOption === 'From' ? '' : selectedFrom?.cc}</h3>
+                        <h3 className='text-2xl sm:text-3xl'>IS EQUIVALENT TO</h3>
+                        <h1 className='my-6 text-4xl font-bold sm:text-7xl'>{result} {toOption === 'To' ? '' : selectedTo?.cc}</h1>
+                        <h4 className='mt-6 sm:mt-0 sm:my-6 text-xl sm:text-xl'> As of {date}</h4>
+                    </div>
+                    <CurrInput
+                        fromInput={fromInput}
+                        currOptions={currOptions}
+                        fromOption={fromOption}
+                        toOption={toOption}
+                        setToOption={setToOption}
+                        setFromInput={setFromInput}
+                        setFromOption={setFromOption}
+                        reverseExchange={reverseExchange}
+                    />
                 </div>
-                <CurrInput
-                    fromInput={fromInput}
-                    currOptions={currOptions}
-                    fromOption={fromOption}
-                    toOption={toOption}
-                    setToOption={setToOption}
-                    setFromInput={setFromInput}
-                    setFromOption={setFromOption}
-                    reverseExchange={reverseExchange}
-                />
             </div>
-        </div>
+        </>
     )
 }
